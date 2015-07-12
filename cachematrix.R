49eb48b49b2e8318f017448d58a037666f2df683
duydt <duydt@fpt.edu.vn>
2015-07-12 12:53:20 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -4,24 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+	 ## equals to NULL to indicate that the matrix is inversed already
+	 ##this, actually look like the data member of class in OOP paradigm 
         isInversed <- NULL
+        ##set value for x also assume that the matrix is inversed 
 	set <- function(b){
-	  a <<- b
+	  x <<- b
    	  isInversed <<- NULL
 	}
+	##return the value of x
 	get <- function(){
-	return (a)
+		return (x)
 	}	
-      setInverse <- function(inverse){isInversed <<- inverse}	
+	##change the value of inverse which means that indicate if current matrix - x is inversed or not
+	setInverse <- function(inverse){
+		isInversed <<- inverse
+	}
+	##get the value of isInversed
 	getInverse <- function()
 	{	
 		return (isInversed)
 	}
+	##list all the members of current class = makeCacheMatrix
 	list (
-	set = set, 
-	get = get,
-	setInverse = setInverse, 
-	getInverse = getInverse
+		set = set, 
+		get = get,
+		setInverse = setInverse, 
+		getInverse = getInverse
 	) 
 }
 
@@ -30,13 +39,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-        	isInversed <- x$getInverse()
+        ##used to check if the matrix has been calculated the inversation or not
+        isInversed <- x$getInverse()
 	##if the isInversed has been calculated
 	if(!is.null(isInversed)){
 		message("getting cached data")
-		isInversed 
+		return (isInversed) 
 	}
-	##calculate inversed, but first get normal matrix name nm 
+	##calculate inversed,
+	##1. get normal matrix of x named nm 
 	nm <- x$get()
 	##calculate and save inversed of nm
 	x$setInverse(solve(nm) %*% nm)
